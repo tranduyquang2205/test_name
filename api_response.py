2b751b1e9c6174a1dbc09d1f251cea30814709a6
tranduyquang <tranduyquang2205@gmail.com>
2024-08-14 10:45:16 +0700

    update

---
+++ api_response.py
@@ -0,0 +1,25 @@
+import json
+from starlette.responses import Response
+
+class APIResponse():
+        def json_format(response,internal_error=False):
+            
+            if internal_error:
+                response = {'code': 500, 'success': False, 'message': response}
+                status_code = 500
+
+            else:
+                if 'code' in response:
+                    print(response)
+                    status_code = response['code']
+                elif 'result' in response:
+                    status_code = 200
+                    response = {'code': 200, 'success': True, 'message': response}
+                else:
+                    response = {'code': 500, 'success': False, 'message': response}
+                    status_code = 500
+                
+            return  Response(content=json.dumps(response),
+                status_code=status_code,
+                media_type="application/json"
+                )
